01b425a63629a0a71be5f924287609925dda4965
anuroopkanna <anuroopindia16@gmail.com>
2023-03-30 08:44:09 +0530

    added:unordered

---
+++ programUnordered1/Node.java
@@ -0,0 +1,11 @@
+package com.BrigdgelabsDsa.programUnordered1;
+
+public class Node <T>{
+    T data;
+    Node<T> next;
+    public Node(T data) {
+        this.data = data;
+    }
+    public Node(){
+    }
+}
